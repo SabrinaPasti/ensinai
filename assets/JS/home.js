2bbf7b6d1da84730996bfe136dc0502a71a83fb1
Sabrina Pasti <bina.pasti123@gmail.com>
2024-11-12 16:17:44 -0300

    Aplicando JavaScript

--- assets/JS/home.js
+++ assets/JS/home.js
@@ -1,6 +1,31 @@
-console.log('teste')
-
-new Splide( '.splide-home', {
-    type   : 'loop',
+new Splide('.splide-home', {
+    type: 'slide',
     perPage: 3,
-  } );
+    perMove: 1,
+    start: 1,
+    pagination: false,
+    gap: '2.2rem',
+    breakpoints: {
+      800: {
+        perPage: 2,
+      },
+      600: {
+        perPage: 1,
+      }
+    }
+}).mount();
+  
+  document.querySelectorAll('.box-texto .open').forEach(img => {
+    img.addEventListener('click', function () {
+  
+      const currentBox = this.closest('.box-texto');
+  
+      document.querySelectorAll('.box-texto').forEach(box => {
+        if (box !== currentBox) {
+          box.classList.remove('show');
+        }
+      });
+  
+      currentBox.classList.toggle('show');
+    });
+});
