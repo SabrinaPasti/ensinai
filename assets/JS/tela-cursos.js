60c32c802a9e70aa05acdf1bdccffae524395f65
MariaEduardaAcyole <dudaacyole12@gmail.com>
2024-11-21 21:26:38 -0300

    adição de atualizações branch Sabrina

---
+++ assets/JS/tela-cursos.js
@@ -0,0 +1,16 @@
+new Splide('.splide__aulas', {
+    type: 'slide',
+    perPage: 3,
+    perMove: 1,
+    start: 1,
+    pagination: false,
+    gap: '2.2rem',
+    breakpoints: {
+      800: {
+        perPage: 2,
+      },
+      600: {
+        perPage: 1,
+      }
+    }
+}).mount();
