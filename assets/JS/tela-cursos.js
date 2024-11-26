bb688a7a71b17e2485410779b9a893409bd0eb2f
lypekaique <lypekaique@hotmail.com>
2024-11-26 02:23:10 -0300

    melhorando reponsividade

--- assets/JS/tela-cursos.js
+++ assets/JS/tela-cursos.js
@@ -1,16 +1,17 @@
-new Splide('.slide__aulas.splide', {
-    type: 'slide',
-    perPage: 3,
-    perMove: 1,
-    pagination: false,
-    gap: '1.3rem',
-    breakpoints: {
-      1100: {
-        perPage: 2,
-      },
-      600: {
-        perPage: 1,
-      }
-    }
+new Splide(".slide__aulas.splide", {
+  type: "slide",
+  perPage: 3,
+  perMove: 1,
+  pagination: false,
+  gap: "1.3rem",
+  breakpoints: {
+    1100: {
+      perPage: 2,
+      arrows: false,
+    },
+    600: {
+      perPage: 1,
+    },
+  },
 }).mount();
 
